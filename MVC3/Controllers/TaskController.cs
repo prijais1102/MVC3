3d0777b4b54a04b2c753573d448fea9f82a3f8c5
prijais1102 <priyajaiswal.mzp1@gmail.com>
2024-01-17 17:33:52 +0530

    Add project files.

---
+++ MVC3/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC3.Models;
+
+namespace MVC3.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Number number = new Number();
+            return View(number);
+        }
+        [HttpPost]
+        public IActionResult Add(Number number) 
+        {
+            ViewBag.res = number.No1 + number.No2;
+            return View();
+
+        }
+    }
+}
---
+++ MVC3/Models/Number.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC3.Models
+{
+    public class Number
+    {
+        public int No1 { get; set; }
+        public int No2 { get; set; }
+    }
+}
